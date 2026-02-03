ed6d9d268ec7b59667761c8f9c2e303f4b1ec65a
Johnny Hall <johnnyhall@safeguardglobal.com>
2026-02-03 13:17:14 +0000

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 
-import react from '@astrojs/react';
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+  integrations: [react(), clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
