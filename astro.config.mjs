a22937bf07924103e85525de9c362da8f321937e
Johnny Hall <johnnyhall@safeguardglobal.com>
2026-03-18 16:16:54 +0000

    Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,7 @@ export default defineConfig({
   adapter: node({ mode: "standalone" }),
   output: "server",
   site: "https://www.peebleswargaming.org",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
