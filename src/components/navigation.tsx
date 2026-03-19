6506f73d937c2ef6a83f7d8e7db3682ec8011184
Johnny Hall <johnnyhall@safeguardglobal.com>
2026-03-19 12:28:21 +0000

    Change nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,7 +2,11 @@
 
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  ShieldCheckIcon,
+} from "@heroicons/react/24/outline";
 import {
   SignedIn,
   SignedOut,
@@ -30,94 +34,92 @@ export default function Navigation() {
     <header className="bg-white">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Peebles Wargaming</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
-          </a>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
+        <div className="flex flex-1">
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm/6 font-semibold text-gray-900 pt-1.5"
+              >
+                {item.name}
+              </a>
+            ))}
             <a
-              key={item.name}
-              href={item.href}
-              className="text-sm/6 font-semibold text-gray-900"
+              href="/join"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              {item.name}
+              Join
             </a>
-          ))}
-        </div>
-        <div className="flex flex-1 items-center justify-end gap-x-6">
-          {/*<SignInButton>
-            <a
-              href="#"
-              className="hidden text-sm/6 font-semibold text-gray-900 lg:block"
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             >
-              Sign In
-            </a>
-          </SignInButton>*/}
-          <a
-            href="./join"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Join
-          </a>
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="submit"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6 cursor-pointer" />
-          </button>
-        </div>
+        <a href="/" className="-m-1.5 p-1.5">
+          <span className="sr-only">Peebles Wargaming</span>
+          <ShieldCheckIcon className="h-8 w-auto" />
+        </a>
+        {/*<div className="flex flex-1 justify-end">
+              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                Sign In <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>*/}
       </nav>
       <Dialog
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
         className="lg:hidden"
       >
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center gap-x-6">
-            <a href="./" className="-m-1.5 p-1.5">
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-1">
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Peebles Wargaming</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
+              <ShieldCheckIcon className="h-8 w-auto" />
             </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
+            {/*<div className="flex flex-1 justify-end">
+              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                Sign In <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>*/}
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
+          <div className="mt-6 space-y-2">
+            <a
+              href="/join"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Join
+            </a>
+            <div className="mt-4">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  {item.name}
+                </a>
+              ))}
             </div>
           </div>
         </DialogPanel>
